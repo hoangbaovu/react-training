97f00b6028d6b12b41201f9dc81af2457c78b83f
Bao Vu Hoang <baovuhbv@gmail.com>
2019-01-11 00:27:17 +0700

    06 Components

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoItem from './components/TodoItem';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <TodoItem />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
---
+++ todo-list/src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TodoItem extends Component {
+  render() {
+    return(
+      <div className="TodoItem">
+        <p>Go Home</p>
+      </div>
+    );
+  }
+}
+
+export default TodoItem;

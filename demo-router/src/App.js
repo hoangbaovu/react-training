8d44a3a98f0ba014784d771271a3f05d443d57cc
Bao Vu Hoang <baovuhbv@gmail.com>
2019-01-13 17:32:16 +0700

    Setup router

--- demo-router/src/App.js
+++ demo-router/src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Contact = () => <h2>Contact</h2>;
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about/">About</Link>
+              </li>
+              <li>
+                <Link to="/contact/">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index} />
+          <Route path="/about/" component={About} />
+          <Route path="/contact/" component={Contact} />
+        </div>
+      </Router>
     );
   }
 }

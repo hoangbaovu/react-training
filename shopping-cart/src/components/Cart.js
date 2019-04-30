aa79336aca34b2e4221f647afdab41b25b3e890e
Bao Vu Hoang <baovuhbv@gmail.com>
2019-04-30 18:42:07 +0700

    Empty Cart

--- shopping-cart/src/components/Cart.js
+++ shopping-cart/src/components/Cart.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Col, Card, CardHeader, CardBody, Table } from 'reactstrap';
 import CartItem from './CartItem';
 
-function Cart() {
+function Cart(props) {
+  let { items } = props;
+
+  const showElementBody = items => {
+    let xhtml = null;
+
+    if (items.length > 0) {
+      xhtml = items.map((cartItem, index) => {
+        return (
+          <CartItem key={index} index={index} cartItem={cartItem} />
+        );
+      });
+    }
+
+    return <tbody id="my-cart-body">{xhtml}</tbody>;
+  }
+
+  const showElementFooter = items => {
+    let xhtml = <tr><th colSpan={6}>Empty product in your cart</th></tr>;
+
+    if (items.length > 0) {
+      xhtml = <tr>
+        <td colSpan={4}>There are <b>5</b> items in your shopping cart.</td>
+        <td colSpan={2} className="total-price text-left">12 USD</td>
+      </tr>
+    }
+    return <tfoot id="my-cart-footer">{xhtml}</tfoot>;
+  }
+
   return (
     <Col md={6}>
       <Card>
@@ -19,18 +48,8 @@ function Cart() {
                 <th>Action</th>
               </tr>
             </thead>
-            <tbody id="my-cart-body">
-              <CartItem />
-              <CartItem />
-              <CartItem />
-            </tbody>
-            <tfoot id="my-cart-footer">
-              <tr><th colSpan={6}>Empty product in your cart</th></tr>
-              <tr>
-                <td colSpan={4}>There are <b>5</b> items in your shopping cart.</td>
-                <td colSpan={2} className="total-price text-left">12 USD</td>
-              </tr>
-            </tfoot>
+            {showElementBody(items)}
+            {showElementFooter(items)}
           </Table>
         </CardBody>
       </Card>
@@ -39,4 +58,10 @@ function Cart() {
   )
 }
 
-export default Cart;
+const mapStateToProps = state => {
+  return {
+    items: state.carts
+  }
+}
+
+export default connect(mapStateToProps, null)(Cart);
